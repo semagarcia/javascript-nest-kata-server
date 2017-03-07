9c5fe8aa9318ad42b97d5312f171aef3573cd779
semagarcia <jmanuelgarciagarcia@gmail.com>
2017-03-07 02:45:57 +0100

    Added express configuration with body-parser

--- src/app.nest.ts
+++ src/app.nest.ts
@@ -1,15 +1,18 @@
 import { NestApplication } from 'nest.js';
+import * as bodyParser from 'body-parser';
 
 export class Application implements NestApplication {
+
     constructor(private expressApp) {
-        // some configuration stuff
+        this.expressApp.use(bodyParser.json());
     }
 
     start(): void {
-        this.expressApp.listen(3030, this.listen);
+        this.expressApp.listen(3000, this.listen);
     }
 
     private listen(): void {
-        console.log('Application listen on port:', 3030);
+        console.log('Application listen on port:', 3000);
     }
+
 }
