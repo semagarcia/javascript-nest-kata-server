a15518da5cfb295782d461f7b59f5e96266adcdc
Sema García <jose-manuel.garcia@gft.com>
2017-03-09 00:38:25 +0100

    Remove comments and innecessary parts

--- src/login/login.component.ts
+++ src/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, HttpException } from 'nest.js';
+const fs = require('fs');
 
 @Component()
 export class LoginService {
@@ -14,30 +15,32 @@ export class LoginService {
     async executeTest(kataFunction: string) {
         console.log('Entrando en executeTest()');
         return new Promise(async(resolve, reject) => {       
-            // Read and update the man file
-            var fs = require('fs');
-            var someFile = './katas/addTwoNumbers/ej.js';
+            // Read and update the exercise file
+            let someFile = './katas/addTwoNumbers/ej.js';
             fs.readFile(someFile, 'utf8', function (err, data) {
-                if (err) {
-                    return console.log(err);
-                }
+                if (err) reject(err);
                 
-                var lastLine = data.substring(data.indexOf('module.exports ='));
-                fs.writeFile(someFile, kataFunction + '\n' + lastLine, 'utf8', function (err) {
-                    if (err) return console.log(err);
+                // Save the last line to preserve it and rewrite it to the file
+                let lastLine = data.substring(data.indexOf('module.exports ='));
+                fs.writeFile(someFile, `${kataFunction}\n${lastLine}`, 'utf8', function (err) {
+                    if (err) reject(err);
 
                     // Execute test
-                    var spawn = require('child_process').spawn;
-                    var npmTest = spawn('npm', ['test', '--prefix', './katas/addTwoNumbers']);
-                    var outputStr = '';
+                    let spawn = require('child_process').spawn;
+                    let npmTest = spawn('npm', ['test', '--prefix', './katas/addTwoNumbers']);
+                    let output = [];
 
+                    // Each line printed by NPM is received and pushed to the array
                     npmTest.stdout.on('data', function (data) {
-                        console.log(data.toString());
-                        outputStr += data;
+                        output.push(data);
                     });
 
+                    // Before to exit, rebuild the output in one single string
                     npmTest.on('close', function (code) {
-                        resolve(outputStr);
+                        // Avoid the two first lines (related to NPM):
+                        // Line[0]: addTwoNumbers@1.0.0 test /path/to/katas/exercise
+                        // Line[1]: ./node_modules/mocha/bin/mocha ej.test.js
+                        resolve(output.slice(2).join(''));
                     });
                 });
             });
