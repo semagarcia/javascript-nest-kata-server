85795e0aad23dd03bf84def908928bdf76ed6dda
Sema García <jose-manuel.garcia@gft.com>
2017-05-24 19:28:56 +0200

    Added new fields like duration, direction and mode

--- src/challenges/challenge.model.ts
+++ src/challenges/challenge.model.ts
@@ -1,6 +1,10 @@
 export class Challenge {
 
     challengeId: string;
+    direction: string;
+    duration: number;
+    mode: string;
+    event: string;
     creator: string;
     playerA: string;
     namePlayerA: string;
@@ -10,10 +14,14 @@ export class Challenge {
     status: ChallengeStatus;
     result: ChallengeResult;
 
-    constructor(id:string, playerId:string) {
+    constructor(id: string, playerId: string, direction: string, duration: number, mode: string, event?: string) {
         this.challengeId = id;
         this.creator = playerId;
+        this.direction = direction;
+        this.duration = duration;
+        this.mode = mode;
         this.timestamp = new Date();
+        this.event = event;
         this.status = ChallengeStatus.WAITING;
     }
 
