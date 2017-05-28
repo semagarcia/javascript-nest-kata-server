973d6ccb4cbfec43e57019fdb80595c3157e04b6
Sema García <jose-manuel.garcia@gft.com>
2017-05-28 05:08:28 +0200

    Added endpoint to recover the ranking without JWT security

--- src/ranking/ranking.controller.ts
+++ src/ranking/ranking.controller.ts
@@ -1,13 +1,15 @@
 import { RequestMethod, Controller, RequestMapping } from 'nest.js';
+import { KatasService } from './../katas/katas.service';
 
-@Controller({ path: 'ranking' })
+@Controller({ path: 'api/ranking' })
 export class RankingController {
 
-    constructor() {}
+    constructor(private katasSrv: KatasService) {}
 
-    @RequestMapping({ path: '', method: RequestMethod.POST })
-    async fn(req, res) { 
-        res.send('ranking root endpoint');
+    @RequestMapping({ path: '', method: RequestMethod.GET })
+    async getRanking(req, res) { 
+        let katasStats = await this.katasSrv.getAllKatasStatistics();
+        res.send({ ranking: katasStats });
     }
 
 }
--- src/ranking/ranking.module.ts
+++ src/ranking/ranking.module.ts
@@ -1,7 +1,9 @@
 import { Module } from 'nest.js';
 import { RankingController } from './ranking.controller';
+import { KatasModule } from './../katas/katas.module';
 
 @Module({
+    modules: [KatasModule],
     controllers: [ RankingController ],
     components: [],
     exports: []
