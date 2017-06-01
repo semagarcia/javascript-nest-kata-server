bd2c61926ff5e52281c25cd062bc441aca85da79
Sema García <jose-manuel.garcia@gft.com>
2017-06-01 23:39:14 +0200

    Added username to JWT token

--- src/login/login.component.ts
+++ src/login/login.component.ts
@@ -11,21 +11,20 @@ export class LoginService {
 
     async standardLogin(username: string, password: string) {
         return new Promise((resolve, reject) => {
-            UserModel.findOne({ username: username }, (err, user: any) => {
+            UserModel.findOne({ username: username }, (err, user: User) => {
                 if(err || !user) reject(`Error at login: ${err}`);
 
                 //if(user && bcrypt.compareSync(password, user.hash)) {
                 if(user) {
                     resolve({ 
                         name: user.name,
-                        // more fields, like next action / operation
+                        username: user.username,                                                                                           
                         token: jwt.sign({ 
                             sub: user._id,  // Identifies token
                             iat: moment().unix(),  // Token creation data,
                             exp: moment().add(60, 'minutes').unix(),  // Expires in 60 minutes
                             // more fields encoded into the token
-                            name: user.name,
-                            email: user.email
+                            name: user.username
                         }, config.secretJwt)
                     });
                 }
