998e1ad330a0c32566b2576be15e4faad5193330
Sema García <jose-manuel.garcia@gft.com>
2017-06-04 02:59:58 +0200

    Updated endpoint URLs

--- src/katas/katas.controller.ts
+++ src/katas/katas.controller.ts
@@ -1,7 +1,7 @@
 import { RequestMethod, Controller, RequestMapping } from 'nest.js';
 import { KatasService } from './katas.service';
 
-@Controller({ path: 'api' })
+@Controller({ path: 'api/katas' })
 export class KatasController {
 
     constructor(private katasSrv: KatasService) {}
@@ -10,17 +10,38 @@ export class KatasController {
      * Endpoint to receive all the katas
      * Path: /api/katas
      */
-    @RequestMapping({ path: 'katas', method: RequestMethod.GET })
+    @RequestMapping({ path: '', method: RequestMethod.GET })
     async getAllKatas(req, res) { 
         let katas = await this.katasSrv.getAllKatas();
         res.send({ katas: katas });
     }
 
     /**
-     * Endpoint to receive the stats of all user/katas
-     * Path: /api/katas
+     * Endpoint to retrieve a kata
+     * Path: /api/katas/kata
      */
-    @RequestMapping({ path: 'katas/stats', method: RequestMethod.GET })
+    @RequestMapping({ path: 'kata/:kataId', method: RequestMethod.GET })
+    async getKataById(req, res) { 
+        await this.katasSrv.getKataById(req.params.kataId)
+            .then(kata => res.send(kata))
+            .catch((err) => res.status(400).send(err));
+    }
+
+    /**
+     * Endpoint to test katas
+     * Path: /api/katas/execute
+     */
+    @RequestMapping({ path: 'execute', method: RequestMethod.POST })
+    async executeKata(req, res) {
+        let kataResult = await this.katasSrv.executeTest(req.body.function, req.body.name);
+        res.status(200).json({ result: kataResult });
+    }
+
+    /**
+     * Endpoint to receive the stats of all user/katas
+     * Path: /api/katas/stats
+     */
+    @RequestMapping({ path: 'stats', method: RequestMethod.GET })
     async getAllKatasStatistics(req, res) { 
         let katasStats = await this.katasSrv.getAllKatasStatistics();
         res.send({ stats: katasStats });
@@ -28,19 +49,9 @@ export class KatasController {
 
     /**
      * Endpoint to test katas
-     * Path: /api/kata
+     * Path: /api/katas/stats/register
      */
-    @RequestMapping({ path: 'kata', method: RequestMethod.POST })
-    async executeKata(req, res) {
-        let kataResult = await this.katasSrv.executeTest(req.body.function, req.body.name);
-        res.status(200).json({ result: kataResult });
-    }
-
-    /**
-     * Endpoint to test katas
-     * Path: /api/kata
-     */
-    @RequestMapping({ path: 'kata/stats', method: RequestMethod.POST })
+    @RequestMapping({ path: 'stats/register', method: RequestMethod.POST })
     async registerKataStats(req, res) {
         let result = await this.katasSrv.updateKataStatistics(req.body.stats, req.session.username, req.session.email);
         res.status(200).json({ result: result });
